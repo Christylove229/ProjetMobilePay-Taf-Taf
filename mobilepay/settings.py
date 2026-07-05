5cafa107533b11b09f6efb30c48009d1c8569c9c
Christylove DOVONON <christylovedovonon@gmail.com>
2026-07-05 13:33:38 +0000

    version1.3

--- mobilepay/settings.py
+++ mobilepay/settings.py
@@ -152,4 +152,25 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
 LOGIN_URL = 'connexion'

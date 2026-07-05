0b800c7601c68d560df766b8d7a3b6005532ef82
Christylove DOVONON <christylovedovonon@gmail.com>
2026-07-05 12:40:26 +0000

    Fix Render root route and static asset path

--- mobilepay/urls.py
+++ mobilepay/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('', lambda request: redirect('connexion')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('accounts/', include('accounts.urls')),
